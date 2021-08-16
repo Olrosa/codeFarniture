54030bebb8e551cd16ffb761cb6f858e75a19b23
Olrosa <64987055+Olrosa@users.noreply.github.com>
2021-08-16 18:12:45 +0300

    Create farnitureScript.js

---
+++ farnitureScript.js
@@ -0,0 +1,8 @@
+const nameList = document.querySelectorAll(".cart-header"),
+const telList = document.querySelectorAll(".show-phones__item");
+telList.forEach((item) => {
+console.log( item.textContent );
+});
+nameList.forEach((item) => {
+console.log( item.textContent );
+});
